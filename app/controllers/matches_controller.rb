389a95a47c00793fbb08d3ee924ec15bbae9dca3
DESKTOP-K6T6FHG\Admin <tophuocthai@gmail.com>
2021-05-16 21:45:40 +0700

    Show all matches of each player

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,7 @@
 class MatchesController < ApplicationController
     def index
-      @matches = Match.all
+      id = params.require(:id)
+      @matches = Player.find(id).matches
     end
   end
   
