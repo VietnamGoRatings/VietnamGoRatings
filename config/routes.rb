e325a4ac9fc65327c87a57c2881aab2435a41147
DESKTOP-K6T6FHG\Admin <tophuocthai@gmail.com>
2021-05-16 09:44:39 +0700

    My first commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'players#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :clubs
+  resources :matches
   resource :users
   namespace :admin do
     resources :clubs do
